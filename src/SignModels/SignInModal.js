6ffe4bf794c88e425eedb33ac404e15b6753d1d2
cluper-LEE <wls4370@gmail.com>
2021-08-29 11:54:54 +0900

    ex

--- src/SignModels/SignInModal.js
+++ src/SignModels/SignInModal.js
@@ -2,6 +2,18 @@ import React from "react";
 import { Modal, Button, Form, Container } from "react-bootstrap";
 import { GoogleLogin } from "react-google-login";
 import HorizontalLine from "../components/HorizonLine";
+import axios from "axios";
+
+let email = '';
+let password = '';
+const onClickLogin = (e) => {
+  axios.post('/auth/login', {email, password}).then((res) => {
+    // 쿠키 받기(로그인) 성공
+    // 로그인 창 닫고 화면 갱신하는 코드
+  }).catch((error) => {
+    console.error(error);
+  })
+}
 
 const SignInModal = ({ show, onHide }) => {
   return (
@@ -20,15 +32,15 @@ const SignInModal = ({ show, onHide }) => {
           <Form>
             <Form.Group>
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control type="email" placeholder="Enter email" onChange={e => email=e.target.value}/>
             </Form.Group>
 
             <Form.Group>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" onChange={e => password=e.target.value}/>
             </Form.Group>
 
-            <Button block variant="info" type="button" className="my-3">
+            <Button block variant="info" type="button" className="my-3" onClick={onClickLogin} >
               Sign In
             </Button>
 
