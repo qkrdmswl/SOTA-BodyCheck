c03772738b731e1687e2de2d80d4f9f0fcc92318
cluper-LEE <wls4370@gmail.com>
2021-08-29 22:35:23 +0900

    Refactor /profile

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -2,24 +2,20 @@ const express = require('express');
 const axios = require('axios');
 const { isLoggedIn, getSuccess, getFailure, verifyPassword } = require('./middleware');
 const { API_URL } = require('../config/const');
+const { getAPI, postAPI, patchAPI } = require('./request');
 const router = express.Router();
 
 
 router.get('/me', isLoggedIn, async (req, res, next) => {
     try {
         const { token, id } = req.user;
-        const profileResult = await axios({
-            method: 'GET',
-            url: `${API_URL}/userProfiles`,
-            headers: { 'bodycheck-access-token': token },
-            params: { UserId: id }
-        });
+        
+        const profileResult = await getAPI('/userProfiles', token, {UserId: id});
 
         return res.status(profileResult.status).json(profileResult.data);
     } catch (err) {
-        const status = err.response.status;
-        if (status === 404 || status === 400) {
-            return res.status(status).json(err.response.data);
+        if(err.response){
+            return res.status(err.response.status).json(err.response.data);
         }
         console.error(err);
         next(err);
@@ -37,24 +33,16 @@ router.post('/me', isLoggedIn, async (req, res, next) => {
             }
         }
 
-        const profileResult = await axios({
-            method: 'POST',
-            url: `${API_URL}/userProfiles`,
-            headers: { 'bodycheck-access-token': token },
-            data: { UserId: id, nick, image, age, height, weight, text },
-        }).catch((err) => {
-            const status = err.response.status;
-            if (status === 404 || status === 400) {
-                return res.status(status).json(err.response.data);
-            }
-            console.error(err);
-            next(err);
+        const profilePostResult = await postAPI('/userProfiles', token, {
+            UserId: id, nick, image, age, height, weight, text,
         });
 
-        return res.status(201).json(profileResult.data);
+        return res.status(201).json(profilePostResult.data);
 
     } catch (err) {
-
+        if(err.response){
+            return res.status(err.response.status).json(err.response.data);
+        }
         console.error(err);
         next(err);
     }
@@ -76,32 +64,19 @@ router.patch('/me', isLoggedIn, async (req, res, next) => {
             return res.status(400).json(getFailure('nick is not nullable'));
         }
 
-        const profileResult = await axios({
-            method: 'GET',
-            url: `${API_URL}/userProfiles`,
-            headers: { 'bodycheck-access-token': token },
-            params: { UserId: id }
-        });
-        const profileId = profileResult.data.data.id;
-        const updateResult = await axios({
-            method: 'PATCH',
-            url: `${API_URL}/userProfiles/${profileId}`,
-            headers: { 'bodycheck-access-token': token },
-            data: { nick, image, age, height, weight, text },
-        }).catch((err) => {
-            const status = err.response.status;
-            if (status === 404 || status === 400) {
-                return res.status(status).json(err.response.data);
-            }
-            console.error(err);
-            next(err);
-        });
+        const profileGetResult = await getAPI('/userProfiles', token, {UserId: id});
+        const profileId = profileGetResult.data.data.id;
 
-        return res.status(updateResult.status).json(updateResult.data);
+        const profilePatchResult = await patchAPI(`/userProfiles/${profileId}`, token, {nick,image,age,height,weight,text});
+
+        return res.status(profilePatchResult.status).json(profilePatchResult.data);
 
     } catch (err) {
-        console.error(error);
-        next(error);
+        if(err.response){
+            return res.status(err.response.status).json(err.response.data);
+        }
+        console.error(err);
+        next(err);
     }
 })
 
