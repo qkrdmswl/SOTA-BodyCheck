edf8f12d268b93c5aae442252b520ff339a89fba
cluper-LEE <wls4370@gmail.com>
2021-08-30 01:40:13 +0900

    Impl /records router

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,183 +1,172 @@
 const express = require('express');
-const { isLoggedIn, getSuccess, getFailure, getValidationError, updateForEach } = require('./middlewares');
-const { Variable, VariableType, Record, DateRecord } = require('../models');
+const { isLoggedIn, getSuccess, getFailure, getValidationError, updateForEach } = require('./middleware');
+const { postAPI, getAPI, patchAPI, deleteAPI } = require('./request');
 const router = express.Router();
 
-router.post('/', isLoggedIn, 
-    async (req, res, next) => {
-        // 만약 요청한 VariableId, RecordId, DateRecordId 값이 없으면 validationError 리턴
-        const { record, VariableId, DateRecordId } = req.body;
-        const params = { record, VariableId, DateRecordId };
-        const validationError = getValidationError(params);
-        if (validationError) {
-            return res.status(400).json(validationError);
-        }
-        else {
-            next();
-        }
-},
-    async (req, res, next) => {
-        const { record, VariableId, DateRecordId } = req.body;
+router.post('/me', isLoggedIn, async (req, res, next) => {
+    try {
+        const {record, set, DateRecordId, VariableId} = req.body;
+        const UserId = req.user.id;
+        const token = req.user.token;
 
-        // 이 부분은 record에서는 어떻게 수정?
-        const exVariable = await Variable.findOne({where: {id:VariableId}});
-        if(!exVariable){ 
-            return res.status(404).json(getFailure(req.originalUrl + ' VariableId'));
+        // DateRecordId 검사
+        const dateRecordGetResult = await getAPI(`/dateRecords/${DateRecordId}`, token);
+        // /me 검사
+        if(UserId != dateRecordGetResult.data.data.UserId){
+            return res.status(400).json(getFailure(req.originalUrl + ' not ur record'));
         }
 
-        const exDateRecord = await DateRecord.findOne({where: {id:DateRecordId}});
-        if(!exDateRecord){ 
-            return res.status(404).json(getFailure(req.originalUrl + ' DateRecordId'));
+        // VariableId 검사
+        const variableGetResult = await getAPI(`/variables/${VariableId}`, token);
+        const exerciseGetResult = await getAPI(`/exercises/${variableGetResult.data.data.ExerciseId}`, token);
+        if(UserId != exerciseGetResult.data.data.UserId){
+            return res.status(400).json(getFailure(req.originalUrl + ' not ur record'));
         }
 
-        const target = await Record.create({
-            record,
-            VariableId,
-            DateRecordId
-        });
-        if(!record){
-            return res.status(500).json(getFailure(`db error: create`));
-        }
+        const recordPostResult = await postAPI('/records', token, {record, set, DateRecordId, VariableId});
 
-        return res.status(201).json(getSuccess(target));
+        return res.status(recordPostResult.status).json(recordPostResult.data);
+    } catch (err) {
+        if(err.response){
+            return res.status(err.response.status).json(err.response.data);
+        }
+        console.error(err);
+        next(err);
+    }
 });
 
-// router.get('/:id')인 것도 있던데 이건 어떤 경우에?
-router.get('/', isLoggedIn, async (req, res, next) => { 
+router.get('/me', isLoggedIn, async (req, res, next) => { 
     try {
-        // query options : { ExerciseId, withRecords }
-        // if (ExerciseId) { return Variables where ExerciseId=ExerciseId } else { return all Variables } 
-        // if (withRecords) { return Variables with Records }
-        const { VariableId, DateRecordId } = req.query;
-        let records;
-        let condition = {};
-        if (VariableId) { // 
-            const variable = await Variable.findOne({
-                where: {
-                    id : VariableId,
-                },
-            })
-            if(!variable) {
-                return res.status(404).json(getFailure(req.originalUrl));
-            }
+        // query options: date, VariableId, DateRecordId, set
+        const token = req.user.token;
+        const UserId = req.user.id;
 
-            // variable 찾기 통과 후
-            condition.VariableId = VariableId;
+        let {date, VariableId, DateRecordId, set} = req.query;
+        let where = {};
+        const dateReg = /^(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])$/;
+
+
+        if(date){
+            if(! dateReg.test(date)){
+                return res.status(400).json(getFailure(req.originalUrl + ' date format error: yyyy-mm-dd'));
+            }
+            const dateRecordGetResult = await getAPI('/dateRecords', token, {UserId, date});
+            DateRecordId = dateRecordGetResult.data.data.id;
         }
 
-        if(DateRecordId){
-            const dateRecord = await DateRecord.findOne({
-                where: {
-                    id : DateRecordId,
-                },
-            })
-            if(!dateRecord) {
-                return res.status(404).json(getFailure(req.originalUrl));
-            }
+        const recordGetResult = await getAPI('/records', token, {VariableId, DateRecordId, set});
 
-            // dateRecord 찾기 통과 후
-            condition.DateRecordId = DateRecordId;
+        return res.status(recordGetResult.status).json(recordGetResult.data);
+
+    } catch (err) {
+        if(err.response){
+            return res.status(err.response.status).json(err.response.data);
+        }
+        console.error(err);
+        next(err);
+    }
+})
+
+
+router.get('/:id/me', isLoggedIn, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const token = req.user.token;
+        const UserId = req.user.id;
+
+        const recordGetResult = await getAPI(`/records/${id}`, token);
+        const dateRecordGetResult = await getAPI(`/dateRecords/${recordGetResult.data.data.DateRecordId}`, token);
+        if(UserId != dateRecordGetResult.data.data.UserId){
+            return res.status(400).json(getFailure(req.originalUrl + ' not ur record'));
         }
 
-        records = await Record.findAll({
-            where: condition,
-        });
+        return res.status(recordGetResult.status).json(recordGetResult.data);
+    } catch (err) {
+        if(err.response){
+            return res.status(err.response.status).json(err.response.data);
+        }
+        console.error(err);
+        next(err);
+    }
+})
+
+router.patch('/:id/me', isLoggedIn, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { record, set } = req.body;
+        const token = req.user.token;
+        const UserId = req.user.id;
+
+        const recordGetResult = await getAPI(`/records/${id}`, token);
+        const dateRecordGetResult = await getAPI(`/dateRecords/${recordGetResult.data.data.DateRecordId}`, token);
+        if(UserId != dateRecordGetResult.data.data.UserId){
+            return res.status(400).json(getFailure(req.originalUrl + ' not ur record'));
+        }
+
+        const recordPatchResult = await patchAPI(`/records/${id}`, token, {record, set});
+        return res.status(recordPatchResult.status).json(recordPatchResult.data);
+
+    } catch (err) {
+        if(err.response){
+            return res.status(err.response.status).json(err.response.data);
+        }
+        console.error(err);
+        next(err);
+    }
+})
+
+
+router.delete('/:id/me', isLoggedIn, async (req, res, next) => {
+    try {
+        // 부모 exercise가 일반삭제 상태고 더이상 레코드가 없으면 exercise, variable 완전삭제
+        const { id } = req.params;
+        const token = req.user.token;
+        const UserId = req.user.id;
         
-        if(records.length === 0){
-            if(VariableId || DateRecordId){
-                return res.status(404).json(getFailure(req.originalUrl));
-            } else {
-                return res.status(204).json();
+        const recordGetResult = await getAPI(`/records/${id}`, token);
+        const dateRecordGetResult = await getAPI(`/dateRecords/${recordGetResult.data.data.DateRecordId}`, token);
+        if(UserId != dateRecordGetResult.data.data.UserId){
+            return res.status(400).json(getFailure(req.originalUrl + ' not ur record'));
+        }
+
+        await deleteAPI(`/records/${id}`, token);
+
+        const record = recordGetResult.data.data;
+        const variableGetResult = await getAPI(`/variables/${record.VariableId}`, token, {paranoid:false});
+        const variable = variableGetResult.data.data;
+        const exerciseGetResult = await getAPI(`/exercises/${variable.ExerciseId}`, token, {paranoid:false});
+        const exercise = exerciseGetResult.data.data;
+
+        if(exercise.deletedAt != null){
+            let exist = false;
+            const variablesGetResult = await getAPI('/variables', token, {
+                ExerciseId: exercise.id, 
+                withRecords:true, 
+                paranoid:false
+            });
+            if(variablesGetResult.status != 204){
+                const variables = variablesGetResult.data.data;
+                for(let i = 0; i < variables.length; i++){
+                    const records = variables[i].Records;
+                    if(records.length != 0){
+                        exist = true;
+                        break;
+                    }
+                }
+                if(!exist){
+                    for(let i = 0; i < variables.length; i++){
+                        await deleteAPI(`/variables/${variables[i].id}`, token, {force: true});
+                    }
+                    await deleteAPI(`/exercises/${exercise.id}`, token, {force: true});
+                }
             }
         }
-
-        return res.status(200).json(getSuccess(records));
-
-    } catch (err) {
-        console.error(err);
-        next(err);
-    }
-})
-
-
-router.get('/:id', isLoggedIn, async (req, res, next) => {
-    try {
-        const { id } = req.params;
-
-        const record = await Record.findOne({ where: { id } });
-        if (!record) {
-            return res.status(404).json(getFailure(req.originalUrl));
-        }
-
-        return res.status(200).json(getSuccess(record));
-    } catch (err) {
-        console.error(err);
-        next(err);
-    }
-})
-
-router.patch('/:id', isLoggedIn, async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const { record, VariableId, DateRecordId } = req.body;
-        // {VariableId, DateRecordId} Not Null
-
-        const target = await Record.findOne({ where: { id } });
-        if (!target) {
-            return res.status(404).json(getFailure(req.originalUrl));
-        }
-
-        // null 검사
-        if(VariableId === null || DateRecordId === null){
-            return res.status(400).json(getFailure(`${req.originalUrl} {VariableId, DateRecordId} Not Null`));
-        }
-
-        // 하나라도 없으면 400 Bad request
-        if(record === undefined && VariableId === undefined && DateRecordId === undefined){
-            return res.status(400).json(getFailure(`${req.originalUrl} At least one content is required`));
-        }
-
-        if(VariableId){ // req.body에 VariableId가 있을 경우 값이 db에 존재하는지 확인 후 업데이트
-            const exVariableId = await Record.findOne({where: {id:VariableId}});
-            if(!exVariableId){
-                return res.status(404).json(getFailure());
-            }
-        }
-
-        if(DateRecordId){ // req.body에 VariableId가 있을 경우 값이 db에 존재하는지 확인 후 업데이트
-            const exDateRecordId = await Record.findOne({where: {id:DateRecordId}});
-            if(!exDateRecordId){
-                return res.status(404).json(getFailure());
-            }
-        }
-
-        // 기존 데이터과 같다면 204 No Contents
-        const isSame = await updateForEach(target, {record, VariableId, DateRecordId});
-        if(isSame){
-            return res.status(204).json();
-        }
-
-        return res.status(201).json(getSuccess(target));
-    } catch (err) {
-        console.error(err);
-        next(err);
-    }
-})
-
-
-router.delete('/:id', isLoggedIn, async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        
-        const record = await Record.findOne({ where: { id } });
-        if (!record) {
-            return res.status(404).json(getFailure(`there is no vairable where id=${id}`));
-        }
-
-        await record.destroy();
 
         return res.status(204).json();
     } catch (err) {
+        if(err.response){
+            return res.status(err.response.status).json(err.response.data);
+        }
         console.error(err);
         next(err);
     }
