1182a53a289e8b56e40fb5e22a389c1610559529
cluper-LEE <wls4370@gmail.com>
2021-08-30 21:33:17 +0900

    Impl login by axios

--- src/SignModels/SignInModal.js
+++ src/SignModels/SignInModal.js
@@ -4,7 +4,19 @@ import { GoogleLogin } from "react-google-login";
 import HorizontalLine from "../components/HorizonLine";
 import FindId from "./FindId";
 import FindPw from "./FindPw";
+import axios from "axios";
 
+let email = '';
+let password = '';
+const onClickLogin = (e) => {
+  axios.post('/auth/login', {email, password}).then((res) => {
+    console.log('asdf');
+    // 쿠키 받기(로그인) 성공
+    // 로그인 창 닫고 화면 갱신하는 코드
+  }).catch((error) => {
+    console.error(error);
+  })
+}
 const SignInModal = ({ show, onHide }) => {
   const [findIdModalOn, setFindIdModalOn] = useState(false);
   const [findPwModalOn, setFindPwModalOn] = useState(false);
@@ -36,15 +48,15 @@ const SignInModal = ({ show, onHide }) => {
           <Form>
             <Form.Group>
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control type="email" placeholder="Enter email" onChange={e => email=e.target.value}/>
             </Form.Group>
 
             <Form.Group>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" onChange={e => password=e.target.value}/>
             </Form.Group>
 
-            <Button block variant="info" type="button" className="my-3" >
+            <Button block variant="info" type="button" className="my-3" onClick={onClickLogin}>
               Sign In
             </Button>
 
